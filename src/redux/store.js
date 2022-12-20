82eea5a0b38a3f2638b6ee18a147fd6507098256
Andrey Zuev <stepgt@mail.ru>
2022-12-20 11:14:18 +1000

    Create posts slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { postReducer } from '../redux/slices/posts';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postReducer,
+  },
 });
 
 export default store;
