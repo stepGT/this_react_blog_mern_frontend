5a41c749ae01d769c85caeeebf6d31e3e02c5062
Andrey Zuev <stepgt@mail.ru>
2023-01-12 09:42:58 +1000

    Added auth reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postReducer } from '../redux/slices/posts';
+import { authReducer } from '../redux/slices/auth';
 
 const store = configureStore({
   reducer: {
     posts: postReducer,
+    auth: authReducer,
   },
 });
 
