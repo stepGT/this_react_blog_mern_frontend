0107d1c7d52e81412352da9fe720d9a273c5f942
Andrey Zuev <stepgt@mail.ru>
2023-01-18 11:50:24 +1000

    Registration && Added fetchRegister slice

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { fetchRegister, isAuthSelector } from '../../redux/slices/auth';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
@@ -8,20 +12,70 @@ import Avatar from '@mui/material/Avatar';
 import styles from './Login.module.scss';
 
 export const Registration = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(isAuthSelector);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      fullName: '',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values));
+    if (!data.payload) {
+      return alert('Не удалось регистрироваться!');
+    }
+
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token);
+    }
+  };
+
+  if (isAuth) return <Navigate to="/" />;
   return (
-    <Paper classes={{ root: styles.root }}>
+    <Paper elevation={0} classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Создание аккаунта
       </Typography>
       <div className={styles.avatar}>
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Зарегистрироваться
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          error={Boolean(errors.fullName?.message)}
+          helperText={errors.fullName?.message}
+          fullWidth
+          {...register('fullName', { required: 'Enter the name!' })}
+          className={styles.field}
+          label="Полное имя"
+        />
+        <TextField
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          fullWidth
+          {...register('email', { required: 'Enter the email!' })}
+          className={styles.field}
+          label="E-Mail"
+        />
+        <TextField
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          fullWidth
+          {...register('password', { required: 'Enter the password!' })}
+          className={styles.field}
+          label="Пароль"
+        />
+        <Button disabled={!isValid} type="submit" size="large" variant="contained" fullWidth>
+          Зарегистрироваться
+        </Button>
+      </form>
     </Paper>
   );
 };
