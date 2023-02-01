89c7a55e987154d0102659bbe3cb8592c235e3de
Andrey Zuev <stepgt@mail.ru>
2023-02-01 10:03:14 +1000

    Added delete post

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -11,6 +11,10 @@ export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
   return data;
 });
 
+export const fetchRemovePost = createAsyncThunk('posts/fetchRemovePost', async (id) =>
+  await axios.delete(`/posts/${id}`),
+);
+
 const initialState = {
   posts: {
     items: [],
@@ -50,6 +54,9 @@ const postSlice = createSlice({
     [fetchTags.rejected]: (state) => {
       state.tags.items = [];
       state.tags.status = 'error';
+    },
+    [fetchRemovePost.pending]: (state, action) => {
+      state.posts.items = state.posts.items.filter((el) => el._id !== action.meta.arg);
     },
   },
 });
